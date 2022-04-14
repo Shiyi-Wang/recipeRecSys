2d721fc454c15f78f9416837f3b4a496614f5b86
Shuangyue Cheng <kat@Shuangyues-MacBook-Pro.local>
2022-04-14 00:28:14 -0500

    move process 1000 to preprocessing notebook

--- app/hybrid_api.py
+++ app/hybrid_api.py
@@ -1,8 +1,28 @@
 import json
 import random
+import pickle
+
+file = open("../data/recipes_names.pkl", 'rb')
+rep_names = pickle.load(file)
 
 with open('../data/top_ingredients.json') as f:
     random_generate_ingredients_range = json.load(f)
+
+
+def translate_recipe_names(results, rep_names=rep_names):
+    print("succeed")
+    return [pretty_text(rep_names[r]) for r in results]
+
+
+def pretty_text(text):
+    ''' This function takes in text and try to put it in a human readable format by putting back \' and making it capitalize
+    '''
+    text = text.replace(" s ", "\'s ")
+    text_split = text.split(" ")
+    # print(text_split)
+    text_split = [t.strip().capitalize() for t in text_split if t != '']
+    # print(text_split)
+    return " ".join(text_split)
 
 
 def generate_a_random_ingredient():
@@ -11,3 +31,4 @@ def generate_a_random_ingredient():
 
 if __name__ == '__main__':
     print(generate_a_random_ingredient())
+    print(translate_recipe_names([23, 56, 34, 33]))
--- hybrid/hybrid.py
+++ hybrid/hybrid.py
@@ -1,3 +1,4 @@
+import random
 knn = ['simple peach pie',
        'gluten free chocolate cake',
        'chevy s salsa   original recipe',
@@ -52,3 +53,42 @@ result: knn_result -> svd_result -> content_based_result
 '''
 
 # three inputs
+
+# suppose we want at the end around 20 recipe
+# this is a feedback from front end
+similar_taste_weight = 0.5
+knn_weight = similar_taste_weight / 2
+svd_weight = similar_taste_weight / 2
+content_based_weight = 1 - knn_weight - svd_weight
+
+# call knn
+knn = ['simple peach pie',
+       'gluten free chocolate cake',
+       'chevy s salsa   original recipe',
+       'hooters buffalo shrimp',
+       'kittencal s greek moussaka']
+knn_selected = random.sample(knn, round(len(knn) * knn_weight))
+print(knn_selected)
+# call svd
+svd = ['Outback Copycat Alice Springs Chicken',
+       'Ensalada Criolla',
+       'Amish Triple Butter Biscuits',
+       'One Pot Chicken Bacon Spinach Parmesan Pasta',
+       'Thai Peanut Coconut Chicken',
+       'Banana Coffee Cake',
+       "Nif's Cajun Macaroni And Cheese",
+       'Coco Oatmeal Honey Cookies',
+       'Creole Watermelon Feta Salad',
+       'Grilled Cajun Green Beans']
+svd_selected = random.sample(knn, round(len(svd) * svd_weight))
+print(svd_selected)
+# call content-based
+# assume it comes back as name of recipes
+content_based = [137739.0, 35397.0, 42195.0, 261482.0, 112444.0]
+# cleaned_content_based = [item for item in content_based if ]
+#
+
+print(random.choices(list(set(['Coco Oatmeal Honey Cookies'])), k=round(
+    len(content_based) * content_based_weight)))
+
+unwanted_ingredients = ['onion', 'garlic']
