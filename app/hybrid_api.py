d5200cc8f0b087c9cc3fa20bf3ecf8aa51d2ab58
Shuangyue Cheng <kat@Shuangyues-MacBook-Pro.local>
2022-04-14 15:32:48 -0500

    finish up description

--- app/hybrid_api.py
+++ app/hybrid_api.py
@@ -1,6 +1,62 @@
 import json
-import random
 import pickle
+
+'''
+This hybrid process mainly works in this way:
+Given three input from the frontend:
+1. I might want ingredients... -> list of ingredients
+2. Ingredients that I absolutely don't want -> list of ingredients
+3. How likely I want to see other users' recipe with a taste similar to me today -> rating bar
+
+Example 1:
+If three input provided by user:
+1. I may be want ingredients: Onions
+2. Ingredients that I absolutely don't want: garlic
+3. similar taste: 0.6
+
+First, we generate three result lists by method 
+pass_to_models(may_want_ingredient='Onions', similar_taste_weight, unwanted_ingredients)
+
+Then, we get the result of knn, svd, and content_based
+
+Second, we combine the results with appropriate weights and other user requirements by method
+combine_results(knn, svd, content_based, similar_taste_weight, unwanted_ingredients):
+    Remove recipes using garlic as ingredient
+    Weight assignment:
+        content_based(onion) -> 0.4
+        knn() -> 0.3
+        svd() -> 0.3
+    final result order: content_based_result(0.4) -> knn_result(0.3) -> svd_result(0.3)
+    Remove duplicate recipes
+
+----------------------------------------------------------------------------------
+Example 2:
+If three input provided by user:
+1. I may be want ingredients: any ingredients works for me
+   -> Random generate from all potential ingredients e.g. tomato, cheese
+2. Ingredients that I absolutely don't want: No -> empty list
+3. similar taste: 0.8
+
+First, if the user is okay with any ingredients, randomly generate a potential 
+ingredient by method generate_a_random_ingredient().
+    e.g. a frontend button "choose one for me" that can randomly generate an 
+    ingredient and fill the textbox. If the user is okay with the generate 
+    result, they can press "looks good to me" button. 
+
+Then, we generate three result lists by method 
+pass_to_models(may_want_ingredient=generated_ingredient, similar_taste_weight, unwanted_ingredients)
+
+Therefore, we get the result of knn, svd, and content_based
+
+Next, we combine the results with appropriate weights and other user requirements by method
+combine_results(knn, svd, content_based, similar_taste_weight, unwanted_ingredients):
+    Remove nothing because no ingredient is specified as unwanted
+    Weight assignment:
+        knn() -> 0.4
+        svd() -> 0.4
+        content_based(tomato, cheese) -> 0.2
+    final result order: knn_result -> svd_result -> content_based_result
+'''
 
 file = open("../data/recipes_names.pkl", 'rb')
 rep_names = pickle.load(file)
@@ -8,10 +64,13 @@ rep_names = pickle.load(file)
 with open('../data/top_ingredients.json') as f:
     random_generate_ingredients_range = json.load(f)
 
-# helper
-
 
 def translate_recipe_names(results, rep_names=rep_names):
+    """ 
+    ** No need to interact with this in front-end
+    This is a helper method used for translating the iid of recipes to 
+    its recipe names for the content based list
+    """
     cleaned_results = set()
     for r in results:
         try:
@@ -24,73 +83,40 @@ def translate_recipe_names(results, rep_names=rep_names):
     cleaned_results = list(cleaned_results)
     return [pretty_text(rep_names[r]) for r in cleaned_results]
 
-# helper
-
 
 def pretty_text(text):
-    ''' This function takes in text and try to put it in a human readable format by putting back \' and making it capitalize
-    '''
+    """
+    ** No need to interact with this in front end
+    This is a helper method used in the process of translating recipe names
+    """
     text = text.replace(" s ", "\'s ")
     text_split = text.split(" ")
     text_split = [t.strip().capitalize() for t in text_split if t != '']
     return " ".join(text_split)
 
-# for generating a random might want ingredient in frontend
 
-
-def generate_a_random_ingredient():
-    return random.choice(random_generate_ingredients_range)
-
-
-'''
-Three input:
-1. I might want ingredients...
-2. Ingredients that I absolutely don't want
-3. How likely I want to see other users' recipe with a taste similar to me today
-
-weight based system on knn, svd, content_based
-If three input provided by user:
-1. I may be want ingredients: Onions
-2. Ingredients that I absolutely don't want: garlic
-3. similar taste: 0.6
-
-**Remove result with garlic
-content_based(onion) -> 0.4
-knn() -> 0.3
-svd() -> 0.3
-
-result order: Content_based_result -> knn_result -> svd_result
-
-----------------------------------------------------------------------------------
-If three input provided by user:
-1. I may be want ingredients: any ingredients works for me
-   -> Random generate from all potential ingredients e.g. tomato, cheese
-2. Ingredients that I absolutely don't want: No
-3. similar taste: 0.8
-
-** No remove step
-knn() -> 0.4
-svd() -> 0.4
-content_based(tomato, cheese) -> 0.2
-
-result: knn_result -> svd_result -> content_based_result
-'''
-
-
-def remove_unwanted_ingredients(combined_list, unwanted_ingredients):
-    copy_combined_list = combined_list.copy()
+def remove_unwanted_ingredients(process_list, unwanted_ingredients):
+    """
+    ** No need to interact with this in front end
+    This is a helper method removing unwanted ingredients in a list
+    """
+    copy_process_list = process_list.copy()
     for unwanted in unwanted_ingredients:
         lowercased_unwanted = str(unwanted).lower()
-        for c in copy_combined_list:
+        for c in copy_process_list:
             words = c.split(" ")
             lower_words = [w.lower() for w in words]
             if lowercased_unwanted in lower_words:
-                combined_list.remove(c)
-        copy_combined_list = combined_list.copy()
+                process_list.remove(c)
+        copy_process_list = process_list.copy()
     return
 
 
 def remove_duplicates(combined_list):
+    """
+    ** No need to interact with this in front end
+    This is a helper method removing all duplicates in the final result list
+    """
     result = []
     for c in combined_list:
         if c not in result:
@@ -98,18 +124,73 @@ def remove_duplicates(combined_list):
     return result
 
 
+def generate_a_random_ingredient():
+    """
+    *** Used in frontend for randomly generate one ingredient ***
+    """
+    return random.choice(random_generate_ingredients_range)
+
+
+def pass_to_models(may_want_ingredient, similar_taste_weight, unwanted_ingredients):
+    """
+    *** Main interact hit point for calling knn, svd, content_based api to 
+    to generate three lists and pass to combine_results(..) for an optimal
+    combined result ***
+
+    Note: 
+    may_want_ingredient should be a list of string values coming from
+    frontend. It is used in calling content based model.
+
+    similar_taste_weight should be a value coming from frontend. The user 
+    drag a rating bar to generate the value. The value is between 0 and 1.
+
+    unwanted_ingredients should be a list of string values coming from 
+    frontend. User specify ingredients they don't want. 
+
+    Args:
+        may_want_ingredient (list): a frontend feedback value
+        similar_taste_weight (double): a frontend feedback value
+        unwanted_ingredients (list): a frontend feedback value
+
+    Returns:
+        list of recipe names: a final recommendation list that should be 
+        sent to the frontend for user to see their recommended recipes by the system.
+    """
+    # not yet finished here
+    knn, svd, content_based = [], [], []
+    # call knn api -> generate one list
+    # call svd api -> generate one list
+    # call contentbased(may_want_ingredient) api -> generate one list
+    return combine_results(knn, svd, content_based, similar_taste_weight, unwanted_ingredients)
+
+
 def combine_results(knn, svd, content_based, similar_taste_weight, unwanted_ingredients):
+    """
+    *** Main interact hit point for generating a hybrid result given three 
+    lists from three models ***
+    Called by pass_to_models
+
+    Args:
+        knn (list): list of recipe names by knn
+        svd (list): list of recipe names by svd
+        content_based (list): list of recipe iids by content based
+        similar_taste_weight (_type_): a frontend feedback value
+        unwanted_ingredients (_type_): a frontend feedback value
+
+    Returns:
+        list of recipe names: a final recommendation list that should be 
+        sent to the frontend for user to see their recommended recipes by the system.
+    """
+
     # calculate weights for different model based on similar_taste_weight specified by the user
     knn_weight = similar_taste_weight / 2
     svd_weight = similar_taste_weight / 2
     content_based_weight = 1 - knn_weight - svd_weight
 
-    # remove "definately unwanted ingredients" from the combined result
+    # remove "definately unwanted ingredients" from all lists
     content_based = translate_recipe_names(content_based)
-    print("--")
-    print(content_based)
-    combined_list = knn + svd + content_based
-    remove_unwanted_ingredients(combined_list, unwanted_ingredients)
+    for process_list in [knn, svd, content_based]:
+        remove_unwanted_ingredients(process_list, unwanted_ingredients)
 
     # select recipes from each list based on the list weight
     # Note we have to make sure we have 10 result from each list
@@ -162,4 +243,4 @@ if __name__ == '__main__':
     '''
     print(";)")
     print(combine_results(knn=knn, svd=svd, content_based=content_based,
-                          similar_taste_weight=0.5, unwanted_ingredients=["buffalo"]))
+                          similar_taste_weight=0.5, unwanted_ingredients=['cake', 'shirmp']))
